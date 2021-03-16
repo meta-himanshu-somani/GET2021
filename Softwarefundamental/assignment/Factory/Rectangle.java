2465732dbe060fab94d42167e7db6893e67ed5dc
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 00:59:10 +0530

    Update Rectangle.java

--- Softwarefundamental/assignment/Factory/Rectangle.java
+++ Softwarefundamental/assignment/Factory/Rectangle.java
@@ -2,9 +2,17 @@
 public class Rectangle implements Shape {
 	private double length=0;
 	private double height=0;
+	private double x1=0;
+	private double x2=0;
+	private double y1=0;
+	private double y2=0;
 	public Rectangle(double LeftBottom_X,double LeftBottom_Y,
 					 double RightUpper_X,double RightUpper_Y) {
 		// TODO Auto-generated constructor stub
+		this.x1=LeftBottom_X;
+		this.x2=RightUpper_X;
+		this.y1=LeftBottom_Y;
+		this.y2=RightUpper_Y;
 		this.length=RightUpper_X-LeftBottom_X;
 		this.height=RightUpper_Y-LeftBottom_Y;
 	}
@@ -23,13 +31,17 @@ public class Rectangle implements Shape {
 	@Override
 	public double getOrigin() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.sqrt(((this.x1-0)*(this.x1-0))+ ((this.y1-0)*(this.y1-0)));
 	}
 
 	@Override
-	public boolean isPointEnclosed() {
+	public boolean isPointEnclosed(double point_x,double point_y) {
 		// TODO Auto-generated method stub
-		return false;
+		if (point_x > this.x1 && point_x < this.x2 &&  
+			    point_y > this.y1 && point_y < this.y2) 
+			    return true; 
+			  
+			return false; 
 	}
 
 }

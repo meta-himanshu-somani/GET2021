f4525c617958d9eac5e11566f4c6a1b87e5788b1
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 00:57:23 +0530

    Update Circle.java

--- Softwarefundamental/assignment/Factory/Circle.java
+++ Softwarefundamental/assignment/Factory/Circle.java
@@ -2,8 +2,12 @@
 public class Circle implements Shape {
 	private double radius=0;
 	private double PI=3.14;
+	private double center_X=0;
+	private double center_Y=0;
 	public Circle(double center_X,double center_Y,double radius) {
 		// TODO Auto-generated constructor stub
+		this.center_X=center_X;
+		this.center_Y=center_Y;
 		this.radius=radius;
 	}
 
@@ -22,13 +26,18 @@ public class Circle implements Shape {
 	@Override
 	public double getOrigin() {
 		// TODO Auto-generated method stub
-		return 0;
+		return Math.sqrt(((this.center_X-0)*(this.center_X-0))+ ((this.center_Y-0)*(this.center_Y-0)))-this.radius;
 	}
 
 	@Override
-	public boolean isPointEnclosed() {
+	public boolean isPointEnclosed(double point_x,double point_y) {
 		// TODO Auto-generated method stub
-		return false;
+		double distance= Math.sqrt(((this.center_X-point_x)*(this.center_X-point_x))+ ((this.center_Y-point_y)*(this.center_Y-point_y)));
+		if(distance>=this.radius){
+			return false;
+		}else {
+			return true;
+		}
 	}
 
 }

65e8a859764e400c99ec8d0521f584c5457bf608
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 00:59:57 +0530

    Update Shape.java

--- Softwarefundamental/assignment/Factory/Shape.java
+++ Softwarefundamental/assignment/Factory/Shape.java
@@ -1,12 +1,30 @@
 
 public interface Shape {
+	/**
+	 * 
+	 * @return area of shape
+	 */
 	public abstract double getArea();
+	/**
+	 * 
+	 * @return perimeter i.e. sum of shape side
+	 */
 	public abstract double getPerimeter();
+	/**
+	 * 
+	 * @return distance from origin to starting point of shape
+	 */
 	public abstract double getOrigin();
-	public abstract boolean isPointEnclosed();
+	/**
+	 * 
+	 * @param point_x X coordinate of shape
+	 * @param point_y Y coordinate of shape 
+	 * @return true if point present in shape else false
+	 */
+	public abstract boolean isPointEnclosed(double point_x,double point_y);
 }
 
-enum ShapeType {
+enum ShapeType{
 	Circle,
 	Rectangle,
 	Triangle,

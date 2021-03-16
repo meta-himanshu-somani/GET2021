09434c9a72feb884def0d0646b4e26271737b5ea
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 00:58:38 +0530

    Update Main.java

--- Softwarefundamental/assignment/Factory/Main.java
+++ Softwarefundamental/assignment/Factory/Main.java
@@ -4,10 +4,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Screen s=new Screen();
-		System.out.println(ShapeType.Circle);
-		System.out.println(s.addShape(ShapeType.Circle, 2.0,2.0,2.0).getArea());
-		System.out.println(s.addShape(ShapeType.Circle, 2.0,2.0,2.0).getArea());
-		System.out.println("remove " + s.deleteShape(ShapeType.Circle));
+		Shape sh=s.addShape(ShapeType.Circle, 2.0,2.0,2.0);
+		System.out.println(sh.getArea());
+		System.out.println(s.addShape(ShapeType.Circle, 4.0,4.0,4.0).getArea());
+		System.out.println(s.addShape(ShapeType.Squre,4,4,6,6).getArea());
+		s.sortShapeBasedOn("Area");
+		//s.deleteAllSpecificShape(ShapeType.Circle);
+		
+		
 	}
 
 }

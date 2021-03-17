3967457f9609a2a9df43f3da00e00fd6faf9cd2f
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 14:14:29 +0530

    Update Counseling.java

--- Softwarefundamental/assignment/Stack ,Queue and Hashing/Counseling.java
+++ Softwarefundamental/assignment/Stack ,Queue and Hashing/Counseling.java
@@ -4,13 +4,24 @@ public class Counseling {
 	CircularQueue studentProgram=new CircularQueue();
 	String[] studentList;
 	String[] studentProgramList;
-	private int capacity(String programName,int[] programCapacity,int totalProgram,String[] nameOfProgram){
+	/**
+	*@param programName program choosen by student
+	*@param programCapacity list of capacity correspond to program that student can choose
+	*@param totalProgram number of program available in college
+	*@param nameOfProgram list of all program name that are available
+	*@return index of program in nameOfProgram if program choosen by student is available else -1
+	*/
+	private int indexOfChoosenProgram(String programName,int totalProgram,String[] nameOfProgram){
 		for(int i=2;i<totalProgram+2;i++){
 			if(programName.equals(nameOfProgram[i])){
 				return i;
 			}
 		}return -1;
 	}
+	/**
+	*@param studentList list of students name 
+	*@param numOfStudents total students for counseling
+	*/
 	public void addStudentList(String[] studentList,int numOfStudents){
 		for(int i=1;i<numOfStudents;i++){
 			students.enqueue(studentList[i]);
@@ -18,13 +29,20 @@ public class Counseling {
 		students.display();
 		this.studentList=students.getQueue();
 	}
-	public void assignProgram(String[][] programPreferList,
+	/**
+	*@param programPreferList list of program preference choosen by students
+	*@param numOfStudents total students for counseling
+	*@param programCapacity list of capacity correspond to program that student can choose
+	*@param totalProgram number of program available in college
+	*@param nameOfProgram list of all program name that are available
+	*/
+	public void assignProgramToStudent(String[][] programPreferList,
 			int numOfStudents,int[] programCapacity,int totalProgram,String[] nameOfProgram){
 		for(int i=1;i<numOfStudents;i++){
 			int flag=0;
 			for(int j=0;j<5;j++){
 				String choosenProgram=programPreferList[i][j];
-				int index=this.capacity(choosenProgram, programCapacity, totalProgram, nameOfProgram);
+				int index=this.indexOfChoosenProgram(choosenProgram, totalProgram, nameOfProgram);
 				if(index!=-1){
 					if(programCapacity[index-2]>0){
 						studentProgram.enqueue(choosenProgram);

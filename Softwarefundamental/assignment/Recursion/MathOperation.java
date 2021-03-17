b54ebcbcebb1df4ef73ee412e72aec43741627a4
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 13:05:37 +0530

    Update MathOperation.java

--- Softwarefundamental/assignment/Recursion/MathOperation.java
+++ Softwarefundamental/assignment/Recursion/MathOperation.java
@@ -4,40 +4,40 @@ public class MathOperation {
 	int j=2;
 	/**
 	 * 
-	 * @param X integer number to find LCM with Y
-	 * @param Y integer number to find LCM with X
-	 * @return LCM of X and Y
+	 * @param X integer number to find LCM with number2
+	 * @param Y integer number to find LCM with number1
+	 * @return LCM of number1 and number2
 	 */
-	public int Lcm(int X,int Y){
-		if(X==1 && Y==1) {
+	public int lcmOfTwoNumbers(int number1,int number2){
+		if(number1==1 && number2==1) {
 		return 1;	
-		}else if (X%i==0 && Y%i==0){
+		}else if (number1%i==0 && number2%i==0){
 			int temp=i;
 			i=2;
-			return temp*Lcm(X/temp,Y/temp);
-		}else if(X%i==0){
+			return temp*lcmOfTwoNumbers(number1/temp,number2/temp);
+		}else if(number1%i==0){
 			int temp=i;
 			i=2;
-			return temp*Lcm(X/temp,Y);
+			return temp*lcmOfTwoNumbers(number1/temp,number2);
 		}else if(Y%i==0){
 			int temp=i;
 			i=2;
-			return temp*Lcm(X,Y/temp);
+			return temp*lcmOfTwoNumbers(number1,number2/temp);
 		}else{
 			i++;
-			return Lcm(X,Y);
+			return lcmOfTwoNumbers(number1,number2);
 		}
 	}
 	/**
 	 * 
-	 * @param X integer number to find HCF/GCD with Y
-	 * @param Y integer number to find HCF/GCD with X
-	 * @return HCF/GCD of X and Y
+	 * @param X integer number to find HCF/GCD with number2
+	 * @param Y integer number to find HCF/GCD with number1
+	 * @return HCF/GCD of number1 and number2
 	 */
-	public int Hcf(int X,int Y){
-		if(X==0){
-			 return Y;
+	public int Hcf(int number1,int number2){
+		if(number1==0){
+			 return number2;
 		}
-		return Hcf(Y%X,X);
+		return Hcf(number2%number1,number1);
 	}
 }

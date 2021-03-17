2c12ddd94984696744c64aff455dec4f5366bd7f
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 11:52:48 +0530

    Add files via upload

---
+++ Softwarefundamental/assignment/Recursion/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Search s= new Search();
+		int[] array={1,2,3,4,5,6,7,8,9,10};
+		System.out.println(s.linearSearch(array, 9));
+
+	}
+
+}

618c569a874573253598917d8abd54c709422756
meta-himanshu-somani <79219903+meta-himanshu-somani@users.noreply.github.com>
2021-03-17 13:07:55 +0530

    Update MathOperationTest.java

--- Softwarefundamental/assignment/Recursion/MathOperationTest.java
+++ Softwarefundamental/assignment/Recursion/MathOperationTest.java
@@ -6,20 +6,20 @@ import org.junit.Test;
 public class MathOperationTest {
 	MathOperation mo =new MathOperation();
 	@Test
-	public void testLcm() {
-		assertEquals(180,mo.Lcm(60, 90) );
-		assertEquals(300,mo.Lcm(60, 100) );
-		assertEquals(1440,mo.Lcm(160, 90) );
-		assertEquals(12540,mo.Lcm(660, 190) );
-		assertEquals(28520,mo.Lcm(620, 920) );
+	public void testLcmOfTwoNumbers() {
+		assertEquals(180,mo.lcmOfTwoNumbers(60, 90) );
+		assertEquals(300,mo.lcmOfTwoNumbers(60, 100) );
+		assertEquals(1440,mo.lcmOfTwoNumbers(160, 90) );
+		assertEquals(12540,mo.lcmOfTwoNumbers(660, 190) );
+		assertEquals(28520,mo.lcmOfTwoNumbers(620, 920) );
 	}
 	@Test
-	public void testHcf(){
-		assertEquals(30,mo.Hcf(60, 90) );
-		assertEquals(20,mo.Hcf(60, 100) );
-		assertEquals(10,mo.Hcf(160, 90) );
-		assertEquals(10,mo.Hcf(660, 190) );
-		assertEquals(20,mo.Hcf(620, 920) );
+	public void testHcfOfTwoNumbers(){
+		assertEquals(30,mo.hcfOfTwoNumbers(60, 90) );
+		assertEquals(20,mo.hcfOfTwoNumbers(60, 100) );
+		assertEquals(10,mo.hcfOfTwoNumbers(160, 90) );
+		assertEquals(10,mo.hcfOfTwoNumbers(660, 190) );
+		assertEquals(20,mo.hcfOfTwoNumbers(620, 920) );
 	}
 
 }
